3616b21d6dc91c052655ebd06eddf8ea24d15943
nightbane30 <andrewpeterson99@gmail.com>
2015-03-26 21:15:15 -0400

    HUUUUUGE UPDATE
    Added spaceship model with player controls.  Press Q/E to rotate your
    ship and WASD to move around.  Press Space to fire bullets.  Music and
    bullet SFX added.  Bullet design is currently a placeholder.  Added
    skybox space background and pause menu.  Press P to pause.  This will
    pause the game and bring up a pause display, as well as pausing all
    active audio.  Other things too that are too lengthy to explain in here,
    BUT WE GOTS OURSELVES THE BASIS TO A GAME!!!

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float moveSpeed;
+	public float turnSpeed;
+	private Transform player;
+	public GameObject bulletSpawn;
+	public GameObject bullet;
+	public float bulletSpeed;
+	public GUIStyle style;
+	public GameObject canvas;
+	public bool canvasActive = false;
+	public AudioSource bgm;
+	public AudioClip laserSFX;
+
+	// Use this for initialization
+	void Start () {
+		player = transform;
+		bgm = bgm.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player.Translate (Vector3.forward * moveSpeed * Time.deltaTime * Input.GetAxis ("Vertical"));
+		player.Translate (Vector3.right * moveSpeed * Time.deltaTime * Input.GetAxis ("Horizontal"));
+		if (Input.GetKey (KeyCode.E)) {
+			transform.Rotate (0, turnSpeed * Time.deltaTime, 0, Space.World);
+		} else if (Input.GetKey (KeyCode.Q)) {
+			transform.Rotate (0, -turnSpeed * Time.deltaTime, 0, Space.World);
+		}
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Shoot();
+		}
+		if (Input.GetKeyDown (KeyCode.P) || Input.GetKeyDown(KeyCode.Escape)) {
+			canvasActive = !canvasActive;
+			Time.timeScale = 0f;
+			canvas.SetActive(canvasActive);
+		}
+		if (canvasActive == true) {
+			bgm.Pause ();
+		} else {
+			Time.timeScale = 1f;
+			bgm.UnPause();
+		}
+	}
+	void Shoot(){
+		GameObject newBullet = (GameObject)Instantiate (bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+		newBullet.GetComponent<Rigidbody> ().AddForce (newBullet.transform.forward * bulletSpeed, ForceMode.VelocityChange);
+		Destroy (newBullet, 10f);
+		bgm.PlayOneShot (laserSFX);
+	}
+}
+
+
+
+
